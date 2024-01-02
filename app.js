573fb73cec8a59244f4c9d733a7b289b48c3ea9b
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-02 19:44:28 +0100

    added api versioning

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/api/v1', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (_, _, next) {
@@ -28,8 +28,9 @@ app.use(function (err, req, res, _) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  code = err.status || 500;
+  res.status(code);
+  res.send(`{ error: ${code} }`)
 });
 
 module.exports = app;
