d56fd31122924fbac3e3560d8e1a6bf36d91911e
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-06 20:42:23 +0100

    feat: added .env for database

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 const { Pool } = require('pg');
 
+require('dotenv').config();
+
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'postgres',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
