2b0b484b1b698130e2a7d09ef57eaec384580d74
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-06 14:06:56 +0100

    feat: testing out the utils

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { limitOffset } = require("../utils");
 
 /* GET home page. */
-router.get('/', function(_, res, _) {
-  res.send("Hello World!")
+router.get("/", function (req, res) {
+  let [limit, offset] = limitOffset(req);
+  res.json({ msg: "Hello World!", limit, offset });
 });
 
 module.exports = router;
