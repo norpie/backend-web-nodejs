1094e94163c81a99a7cbfb144bfa37cfecd786c2
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-07 10:35:25 +0100

    feat: function to get user_id from jwt

--- utils.js
+++ utils.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const { getConnection } = require('./db');
+
 function clamp(value, min, max) {
   return Math.min(Math.max(value, min), max);
 }
@@ -15,7 +18,32 @@ function getSecret() {
   return process.env.API_SECRET;
 }
 
+async function userId(req) {
+  const token = req.headers['authorization'];
+  console.log(req.headers);
+  if (!token) {
+    throw new Error('No token provided');
+  }
+  const secret = getSecret();
+  const decoded = await jwt.verify(token, secret);
+  if (!decoded) {
+    throw new Error('Invalid token');
+  }
+  const query = {
+    text: 'SELECT * FROM api_sessions WHERE id = $1',
+    values: [decoded.id],
+  };
+  const connection = await getConnection();
+  const result = await connection.query(query);
+  if (result.rows.length === 0) {
+    throw new Error('Invalid token');
+  }
+  const session = result.rows[0];
+  return session.user_id;
+}
+
 module.exports = {
   limitOffset,
   getSecret,
+  userId,
 };
